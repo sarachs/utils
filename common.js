dc919185de42d4332c252ab3cb8e06304b7f170f
sarachs <77602200+sarachs@users.noreply.github.com>
2022-05-24 09:18:11 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,1 +1,20 @@
-const a = 20;
+/**
+ * 创建异步函数
+ * @param {Boolean} type   - 异步函数类型 true resolve | false reject
+ * @param {String} content - 内容
+ * @param {Number} delay   - 延时时间 默认 1000ms
+ * @returns
+ */
+function createAsyncFn(type = true, content = "hello", delay = 1000) {
+    return new Promise((resolve, reject) => {
+        console.log(`new Promise()`);
+        setTimeout(() => {
+            console.log(`创建了异步函数 ${new Date().toLocaleString()}`);
+            if (type) {
+                return resolve(content);
+            } else {
+                return reject(content);
+            }
+        }, delay);
+    });
+}
