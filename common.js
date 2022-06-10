f1eca58f64d2797ad03190600b2ecf294dd35a32
sarachs <77602200+sarachs@users.noreply.github.com>
2022-06-10 08:46:23 +0800

    Update common.js

--- common.js
+++ common.js
@@ -68,3 +68,15 @@ function handlerClick1001() {
     }
     console.log("正常执行代码了");
 }
+
+/**
+ * 得到一个两数之间的随机整数，包括两个数在内
+ * @param {Number} min 最小值
+ * @param {Number} max 最大值
+ * @returns {Number} 随机数
+ */
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
